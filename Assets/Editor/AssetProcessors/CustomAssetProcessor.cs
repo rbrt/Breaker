26b11d5a70c9c2395afd585ce866f57c277d2aba
Rob Butler <robert@silverbackgames.com>
2016-02-26 18:15:22 -0400

    Added asset post processor and player character

---
+++ Assets/Editor/AssetProcessors/CustomAssetProcessor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.IO;
+
+
+public class CustomAssetProcessor : AssetPostprocessor {
+
+	void OnPreprocessModel(){
+		var fileNameIndex = assetPath.LastIndexOf('/');
+        var fileName = assetPath.Substring(fileNameIndex + 1);
+
+        var importer = assetImporter as ModelImporter;
+
+        importer.importMaterials = false;
+        importer.optimizeMesh = true;
+	}
+
+}
