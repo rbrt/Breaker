64faf81644b85d57d3bae035c6e265e7dc43d227
Rob Butler <robert@silverbackgames.com>
2016-03-05 14:50:40 -0400

    Multiplier works. Added visual indication of multiplier timer

--- Assets/Scripts/GUI/ScoreDisplay.cs
+++ Assets/Scripts/GUI/ScoreDisplay.cs
@@ -13,6 +13,8 @@ public class ScoreDisplay : MonoBehaviour {
 
 	const int scoreLength = 8;
 
+	const float multiplierLifetime = 15;
+
 	static ScoreDisplay instance;
 
 	public static ScoreDisplay Instance {
@@ -23,17 +25,30 @@ public class ScoreDisplay : MonoBehaviour {
 
 	[SerializeField] protected Text scoreText;
 	[SerializeField] protected Text multiplierText;
+	[SerializeField] protected Image multiplierMask;
 
 	int currentScore = 0;
 	int actualScore = 0;
 
+	int multiplier = 1;
+
 	string scoreString = "000000000";
 	string multiplierString = "0x";
+
+	float lastScoreTime = 0;
+	int lastTierScore;
+
+	int[] scoreTiers = {0,
+						20,
+						70,
+						160,
+						250};
 
 	void Awake(){
 		instance = this;
 		scoreText.text = scoreString;
 		multiplierText.text = multiplierString;
+		lastScoreTime = Time.time;
 	}
 
 	void Update () {
@@ -47,8 +62,31 @@ public class ScoreDisplay : MonoBehaviour {
 			else{
 				currentScore++;
 			}
-			
+
 			scoreText.text = ConvertScoreToString(currentScore);
+		}
+
+
+		if (Time.time - lastScoreTime > multiplierLifetime){
+			if (multiplier > 1){
+				multiplier--;
+				lastScoreTime = Time.time;
+				lastTierScore = 0;
+			}
+		}
+		else if (multiplier < (scoreTiers.Length - 1) && lastTierScore > scoreTiers[multiplier]){
+			multiplier++;
+			lastScoreTime = Time.time;
+			lastTierScore = 0;
+		}
+
+		multiplierText.text = "x" + multiplier;
+
+		if (multiplier > 1){
+			multiplierMask.fillAmount = 1 - ((Time.time - lastScoreTime) / multiplierLifetime);
+		}
+		else {
+			multiplierMask.fillAmount = 0;
 		}
 	}
 
@@ -63,6 +101,8 @@ public class ScoreDisplay : MonoBehaviour {
 	}
 
 	public void AddScore(int points){
+		points *= multiplier;
 		actualScore += points;
+		lastTierScore += points;
 	}
 }
