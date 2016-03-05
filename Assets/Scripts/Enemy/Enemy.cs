0d5be97dd6dbe2a01b21c997f1c63c2786769d4d
Rob Butler <robert@silverbackgames.com>
2016-03-05 17:47:32 -0400

    Added tank enemy model to basic enemy type

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -10,7 +10,15 @@ public class Enemy : MonoBehaviour {
 	[SerializeField] protected int contactDamage = -1;
 	[SerializeField] protected int shieldReward = 5;
 
-	float shotRate = 50f;
+	[SerializeField] protected Animator turretAnimator;
+	[SerializeField] protected ParticleSystem shotChargeParticles;
+
+	[SerializeField] protected Transform turretTransform;
+
+	float idleShotParticleSize = .1f;
+	float attackingShotParticleSize = .6f;
+
+	float shotRate = 35f;
 
 	bool dying = false;
 
@@ -26,6 +34,8 @@ public class Enemy : MonoBehaviour {
 	}
 
 	void Update(){
+		turretTransform.LookAt(playerTransform);
+
 		var point = CameraController.ScreenPoint(transform.position + Vector3.right * (transform.localScale.x / 2));
 		if (point.x + (transform.localScale.x / 2) < -10){
 			EnemySpawner.Instance.ClearEnemy(this);
@@ -59,14 +69,45 @@ public class Enemy : MonoBehaviour {
 	}
 
 	IEnumerator ShootAtPlayer(){
+		yield return new WaitForSeconds(Random.Range(0, .9f));
+		
 		while (true && !PlayerController.Instance.Dead){
 			if (!DebugMenu.EnemiesDontAttack){
+				shotChargeParticles.startSize = idleShotParticleSize;
+
 				yield return new WaitForSeconds(shotRate * Time.deltaTime);
+
+				for (float i = 0; i < 1; i += Time.deltaTime / .2f){
+					shotChargeParticles.startSize = Mathf.Lerp(idleShotParticleSize, attackingShotParticleSize, i);
+					yield return null;
+				}
+
+				for (float i = 0; i < 1; i += Time.deltaTime / .2f){
+					turretAnimator.speed = Mathf.Lerp(1, 0, i);
+					yield return null;
+				}
+
+				shotChargeParticles.Stop();
+
 				var shot = GameObject.Instantiate(shotPrefab.gameObject);
-				Vector3 pos = transform.position + (playerTransform.position - transform.position).normalized * .5f;
+				Vector3 pos = shotChargeParticles.transform.position; //+ (playerTransform.position - transform.position).normalized * .5f;
 				pos.z = 0;
 
 				shot.transform.position = pos;
+
+				for (float i = 0; i < 1; i += Time.deltaTime / .05f){
+					shotChargeParticles.startSize = Mathf.Lerp(attackingShotParticleSize, idleShotParticleSize, i);
+					yield return null;
+				}
+
+				shotChargeParticles.Play();
+
+				for (float i = 0; i < 1; i += Time.deltaTime / .05f){
+					turretAnimator.speed = Mathf.Lerp(0, 1, i);
+					yield return null;
+				}
+
+
 			}
 			yield return null;
 		}
